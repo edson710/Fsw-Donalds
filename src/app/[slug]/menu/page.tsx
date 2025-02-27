044600047e03c22f7a149c201cc5f006b8c36b97
Edson <edson0195@gmail.com>
2025-02-27 19:25:40 -0300

    Primeiro commit

---
+++ src/app/[slug]/menu/page.tsx
@@ -0,0 +1,78 @@
+import { ChevronLeftIcon, ScrollTextIcon } from "lucide-react";
+import Image from "next/image";
+import { notFound } from "next/navigation";
+
+import { Button } from "@/components/ui/button";
+import { db } from "@/lib/prisma";
+
+import RestaurantCategories from "./components/categories";
+import RestaurantHeader from "./components/header";
+
+interface RestaurantMenuPageProps {
+  params: { slug: string };
+  searchParams: { consumptionMethod?: string };
+}
+
+const isConsumptionMethodValid = (consumptionMethod?: string) => {
+  return ["DINE_IN", "TAKEWAY"].includes(
+    consumptionMethod?.toUpperCase() || "",
+  );
+};
+
+const RestaurantMenuPage = async ({
+  params,
+  searchParams,
+}: RestaurantMenuPageProps) => {
+  const { slug } = params;
+  const consumptionMethod = searchParams.consumptionMethod;
+
+  if (!isConsumptionMethodValid(consumptionMethod)) {
+    return notFound();
+  }
+
+  const restaurant = await db.restaurant.findUnique({
+    where: { slug },
+    include: {
+      menuCategories: {
+        include: {products: true},
+      },
+    },
+  });
+  if (!restaurant) {
+    return notFound();
+  }
+
+  return (
+    <div className="relative h-[250px] w-full">
+      <Button
+        variant="secondary"
+        size="icon"
+        className="absolute left-4 top-4 z-50 rounded-full"
+      >
+        <ChevronLeftIcon />
+      </Button>
+
+      {restaurant.coverImageUrl && (
+        <Image
+          src={restaurant.coverImageUrl}
+          alt={restaurant.name}
+          fill
+          className="object-cover"
+        />
+      )}
+
+      <Button
+        variant="secondary"
+        size="icon"
+        className="absolute right-4 top-4 z-50 rounded-full"
+      >
+        <ScrollTextIcon />
+      </Button>
+      <RestaurantHeader restaurant={restaurant} />
+      <RestaurantCategories restaurant={restaurant} />
+    </div>
+  );
+};
+
+export default RestaurantMenuPage;
+
---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+const ProductPage = () => {
+  return (
+  
+  <div className="p-5 border border-red-500 rounded-xl">
+  <h1 className="text-red-500">products page</h1>
+  <button>fs</button>
+  <input placeholder="bora fechar"/>
+  </div>
+  );
+};
+
+  export default ProductPage;
