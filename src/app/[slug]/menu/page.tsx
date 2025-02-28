a960f19adf1db7866b9d1d9954602d06d06ce9d3
Edson <edson0195@gmail.com>
2025-02-28 13:35:51 -0300

    feat: add initial cart context and cart sheet

--- src/app/[slug]/menu/page.tsx
+++ src/app/[slug]/menu/page.tsx
@@ -30,54 +30,49 @@ const RestaurantMenuPage = async ({
     return notFound();
   }
 
-  try {
-    const restaurant = await db.restaurant.findUnique({
-      where: { slug },
-      include: {
-        menuCategories: {
-          include: { products: true },
-        },
+  const restaurant = await db.restaurant.findUnique({
+    where: { slug },
+    include: {
+      menuCategories: {
+        include: {products: true},
       },
-    });
-
-    if (!restaurant) {
-      return notFound();
-    }
-
-    return (
-      <div className="relative h-[250px] w-full">
-        <Button
-          variant="secondary"
-          size="icon"
-          className="absolute left-4 top-4 z-50 rounded-full"
-        >
-          <ChevronLeftIcon />
-        </Button>
-
-        {restaurant.coverImageUrl && (
-          <Image
-            src={restaurant.coverImageUrl}
-            alt={restaurant.name}
-            fill
-            className="object-cover"
-          />
-        )}
-
-        <Button
-          variant="secondary"
-          size="icon"
-          className="absolute right-4 top-4 z-50 rounded-full"
-        >
-          <ScrollTextIcon />
-        </Button>
-        <RestaurantHeader restaurant={restaurant} />
-        <RestaurantCategories restaurant={restaurant} />
-      </div>
-    );
-  } catch (error) {
-    console.error("Erro ao buscar restaurante:", error);
+    },
+  });
+  if (!restaurant) {
     return notFound();
   }
+
+  return (
+    <div className="relative h-[250px] w-full">
+      <Button
+        variant="secondary"
+        size="icon"
+        className="absolute left-4 top-4 z-50 rounded-full"
+      >
+        <ChevronLeftIcon />
+      </Button>
+
+      {restaurant.coverImageUrl && (
+        <Image
+          src={restaurant.coverImageUrl}
+          alt={restaurant.name}
+          fill
+          className="object-cover"
+        />
+      )}
+
+      <Button
+        variant="secondary"
+        size="icon"
+        className="absolute right-4 top-4 z-50 rounded-full"
+      >
+        <ScrollTextIcon />
+      </Button>
+      <RestaurantHeader restaurant={restaurant} />
+      <RestaurantCategories restaurant={restaurant} />
+    </div>
+  );
 };
 
 export default RestaurantMenuPage;
+
